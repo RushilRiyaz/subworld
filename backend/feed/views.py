3d1df515bbbdb1ab36b795a0cec7bf518b893895
Rushil Riyaz <rushilriyazzz@gmail.com>
2025-04-20 20:49:16 +0200

    implemented pagination and user posts page

--- backend/feed/views.py
+++ backend/feed/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User 
 
 
 # List View
@@ -10,6 +11,7 @@ class PostListView(ListView):
     template_name = 'feed/home.html' # expects <app>/<model>_<viewtype>.html | feed/post_list.html 
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 
 # Detail view for each post
@@ -52,3 +54,16 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
+    
+
+# List View for a specific user
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'feed/user_posts.html'
+    context_object_name = 'posts'
+    
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')  
