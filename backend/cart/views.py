a5f0b73cf54ab5c9c8174f6815e9d6fdb2b00fb9
Rushil Riyaz <rushilriyazzz@gmail.com>
2025-04-23 20:59:14 +0200

    feat: add different quantities of a single sandwich to cart

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -6,8 +6,9 @@ from django.http import JsonResponse
 def cart_summary(request):
 	# get cart
 	cart = Cart(request)
-	cart_products = cart.get_prods()
-	return render(request, "cart/cart_summary.html", {"cart_products": cart_products})
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	return render(request, "cart/cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
 
 def cart_add(request):
 	# Get the cart
@@ -16,12 +17,13 @@ def cart_add(request):
 	if request.POST.get('action') == 'post':
 		# Get stuff
 		product_id = int(request.POST.get('product_id'))
+		product_qty = int(request.POST.get('product_qty'))
 
 		# lookup product in DB
 		product = get_object_or_404(Post, id=product_id)
 		
 		# Save to session
-		cart.add(product=product)
+		cart.add(product=product, quantity=product_qty)
 
 		# Get cart quantity
 		cart_quanity = cart.__len__()
